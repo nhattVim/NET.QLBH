16442fa110d3a94cbe64650eeea48666b359a4a5
nhattVim <nhattruong13112000@gmail.com>
2025-04-10 14:15:46 +0700

    add API Request Collection

--- Utils/cmd.ps1
+++ Utils/cmd.ps1
@@ -1,4 +1,7 @@
 dotnet ef dbcontext scaffold "Server=172.28.182.33;Database=QLBH;User Id=sa;Password=nhattVim2*;TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer -o Models --force
 
 dotnet aspnet-codegenerator controller -name ProductController -api -m Product -dc QlbhContext -outDir Controllers --useAsyncActions
+
 dotnet aspnet-codegenerator controller -name UserController -api -m User -dc QlbhContext -outDir Controllers --useAsyncActions
+
+httpgenerator http://localhost:5000/swagger/v1/swagger.json -o Utils/Http
